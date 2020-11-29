bc1cac0fe639ae91befcf73c5f210af0244c00f8
thangdinh1298 <thangdinh.umbala@gmail.com>
2020-11-29 23:58:34 +0700

    WIP: Form to update overview and info

---
+++ static/js/update_page.js
@@ -0,0 +1,45 @@
+function openTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
+
+function updateWithTextArea(div_id, button_id, url) {
+   let div = document.getElementById(div_id);
+   let button = document.querySelector("#" + div_id + " #" + button_id)
+   let delete_form = document.querySelector("#" + div_id + " form")
+   
+   button.remove()
+
+   let form = document.createElement("form");
+   form.action=url
+   form.method="POST"
+   let text_area = document.createElement("textarea");
+   button = document.createElement("button");
+   button.type = "submit"
+   button.textContent = "Update"
+
+   form.appendChild(text_area)
+   form.appendChild(button)
+   div.insertBefore(form, delete_form)
+   tinymce.init({ selector: "#" + div_id + " form textarea"});
+}
